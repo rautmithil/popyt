ada23431f682c8b3c5776d1c322fff281f5b5e8f
rautmithil <rautmithil@gmail.com>
2017-04-15 19:21:54 -0400

    loadcell calibration updated

--- src/popyt/popyt.ino
+++ src/popyt/popyt.ino
@@ -3,14 +3,33 @@
 #include <HMC5883L.h> // HMC5883L Magnetometer Library
 #include <ITG3200.h>
 #include <HX711.h>
+#include <LiquidCrystal.h>
 
-// Connections on board
-HX711 scale(A1, A0); // Weight scale sensor
+// Connections on 
+// HX711.DOUT  - pin #A2
+// HX711.PD_SCK - pin #A1
+HX711 scale(A1, A2); // Weight scale sensor
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);  //LCD initalization
 
 //Variables of each on-board sensor
 ADXL345 acc; //variable acc is an instance of the ADXL345 library
 HMC5883L compass; //variable compass is an instance of the HMC5883L library
 ITG3200 gyro = ITG3200(); //variable gyro is an instance of the ITG3200 library
+float  gx,gy,gz;
+float  gx_rate, gy_rate, gz_rate;
+int ix, iy, iz;
+float anglegx=0.0, anglegy=0.0, anglegz=0.0;
+int ax,ay,az;  
+int rawX, rawY, rawZ;
+float X, Y, Z;
+float rollrad, pitchrad;
+float rolldeg, pitchdeg;
+float headingDegrees = 0.0;
+int error = 0; 
+float aoffsetX, aoffsetY, aoffsetZ;
+float goffsetX, goffsetY, goffsetZ;
+unsigned long time, looptime;
+float weight;
 
 //Physique parameters
 int height = 180;
@@ -18,7 +37,17 @@ int height = 180;
 //Variables for parameters
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  //Output for HX711 module
+  pinMode(A0,OUTPUT);
+  digitalWrite(A0,LOW);
+  pinMode(A3,OUTPUT);
+  digitalWrite(A3,HIGH);
+
+  //LCD Backlight
+  pinMode(8,OUTPUT);
+  digitalWrite(8,HIGH);
+  Serial.begin(115200);
+  calibrate_force();
   acc.powerOn();
   compass = HMC5883L();
   
@@ -47,9 +76,9 @@ void setup() {
 }
 
 void loop() {
-
-calculate_roll_pitch();
+calculate_accel();
 calculate_compass();
+
 //calculate_gyro();
 
 Serial.print(rolldeg);
@@ -58,14 +87,15 @@ Serial.print(pitchdeg);  // calculated angle in degrees
 Serial.print(",");
 Serial.print(headingDegrees);
 Serial.println(",");
-
-
-
+delay(1000);
+measure_force();
 
 }
 
+
+
 // code fragment for Accelerometer angles (roll and pitch)
-void calculate_accel{
+void calculate_accel(){
   time = millis();
   acc.readAccel(&ax, &ay, &az); //read the accelerometer values and store them in variables  x,y,z
   rawX = ax - aoffsetX;
@@ -92,7 +122,7 @@ void calculate_compass(){
     heading += 2*PI;
   if(heading > 2*PI)
     heading -= 2*PI;
-  float headingDegrees = heading * 180/M_PI;
+  headingDegrees = heading * 180/M_PI;
 }
 
 // Code fragment for Gyroscope (roll, pitch, yaw)  
@@ -115,19 +145,73 @@ void calculate_gyro(){
   anglegz = anglegz + (gz_rate * looptime);
 }
 
-void measure_orientation(){
-  
-}
+
+
 void measure_force(){
-  scale.read();
+  //Serial.print("one reading:\t");
+  //Serial.print(scale.get_units(), 1);
+  //Serial.print("\t| Weight:\t");
+  float wei = scale.get_units(10);
+  float weight = floor((float)(37.273 * (float)(wei)) - 0.48)/1000;
+  //float weight = (scale.get_units(5));
+  Serial.print(weight);
+  Serial.print("          ");
+  lcd.clear();
+  lcd.setCursor(2,2);
+  lcd.print(weight);
+  scale.power_down();              // put the ADC in sleep mode
+  delay(100);
+  scale.power_up();
 }
+
+
 
 void count_reps(){
   
 }
 
+
 void display_reps(){
   
+}
+
+
+void calibrate_force(){
+  Serial.println("HX711 Demo");
+
+  Serial.println("Before setting up the scale:");
+  Serial.print("read: \t\t");
+  Serial.println(scale.read());      // print a raw reading from the ADC
+
+  Serial.print("read average: \t\t");
+  Serial.println(scale.read_average(20));   // print the average of 20 readings from the ADC
+
+  Serial.print("get value: \t\t");
+  Serial.println(scale.get_value(5));   // print the average of 5 readings from the ADC minus the tare weight (not set yet)
+
+  Serial.print("get units: \t\t");
+  Serial.println(scale.get_units(5), 1);  // print the average of 5 readings from the ADC minus tare weight (not set) divided 
+            // by the SCALE parameter (not set yet)  
+
+  scale.set_scale(2280.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
+  scale.tare();               // reset the scale to 0
+
+  Serial.println("After setting up the scale:");
+
+  Serial.print("read: \t\t");
+  Serial.println(scale.read());                 // print a raw reading from the ADC
+
+  Serial.print("read average: \t\t");
+  Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
+
+  Serial.print("get value: \t\t");
+  Serial.println(scale.get_value(5));   // print the average of 5 readings from the ADC minus the tare weight, set with tare()
+
+  Serial.print("get units: \t\t");
+  Serial.println(scale.get_units(5), 1);        // print the average of 5 readings from the ADC minus tare weight, divided 
+            // by the SCALE parameter set with set_scale
+
+  Serial.println("Readings:");
 }
 
 
