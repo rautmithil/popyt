c8d8af6f8eb1f3c0235dcdf2bf1eb424297ba454
rautmithil <rautmithil@gmail.com>
2017-04-26 10:21:34 -0400

    Revert "res"
    This reverts commit df5483ef1e372b7a2b81e60e14e241c93ebf9ab3.

--- src/popyt/popyt.ino
+++ src/popyt/popyt.ino
@@ -33,7 +33,6 @@ float weight;
 static float threshold = 0.0;
 static int RepCount = 0;
 static boolean RepCountFlag = true;
-float hori_angle, vert_angle;
 
 //Physique parameters
 int height = 180;
@@ -86,16 +85,7 @@ void setup() {
 void loop() {
 calculate_accel();
 calculate_compass();
-//Serial.print(rolldeg);
-//Serial.print(",");
-calc_angle();
-Serial.print("Angle Vert: ");
-Serial.print(vert_angle);
-Serial.println(" , ");
-//Serial.print(pitchdeg);
-//Serial.print(",");
-//Serial.print(headingDegrees);
-//Serial.println(",");
+
 //calculate_gyro();
 //Print data
 
@@ -108,9 +98,6 @@ lcd.println(",");*/
 //lcd.setCursor(0,2);
 //Serial.print(RepCount);
 count_reps();
-lcd.setCursor(0,4);
-lcd.print("Threshold: ");
-lcd.print(threshold);
 delay(100);
 }
 
@@ -175,23 +162,15 @@ float measure_force(){
   //Serial.print("\t| Weight:\t");
   float wei = scale.get_units(10);
   float weight = floor((float)(37.273 * (float)(wei)))/1000;
-  if(weight<0)
-  {
-    weight = 0.0;
-  }
   //float weight = (scale.get_units(5));
-  Serial.print("Force: ");
-  Serial.print(weight);
+  Serial.println(weight);
   Serial.println("          ");
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("Force: ");
   lcd.print(weight);
-  calc_angle();
   lcd.setCursor(0,2);
   lcd.print("Reps: ");
-  Serial.print("Reps: ");
-  Serial.println(RepCount);
   lcd.print(RepCount);
   //lcd.print(",");
   return weight;
@@ -203,7 +182,7 @@ float measure_force(){
 
 
 void count_reps(){
-  if(measure_force() > (float)threshold){
+  if(measure_force() > (float)threshold && RepCountFlag == true){
     RepCount ++;
     Serial.print("Reps: ");
     Serial.println(RepCount);
@@ -219,13 +198,8 @@ void count_reps(){
   //return RepCount;
 }
 
-void calc_angle(){
-  vert_angle = rolldeg*(-1);
-  lcd.setCursor(0,1);
-  lcd.print("Angle Vert: ");
-  lcd.print(vert_angle);
-  //hori_angle = 
-  //Serial.print(hori_angle);
+
+void display_reps(){
   
 }
 
@@ -234,7 +208,7 @@ void input_force(){
   lcd.print("Input force in   Kg");
   while(Serial.available() == 0){}
   if (Serial.available() > 0) {
-    threshold = Serial.parseFloat();
+    threshold = Serial.parseInt();
     Serial.print(threshold);
     lcd.setCursor(0,0);
     lcd.print("Received force:");
